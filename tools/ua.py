b9ff8f26bb8045e40e6b628289bad8cbd0efe325
bernard <bernard.xiong@gmail.com>
2015-02-07 20:37:19 +0800

    Add VC++ compiler support.

--- tools/ua.py
+++ tools/ua.py
@@ -77,18 +77,38 @@ def BuildApplication(TARGET, SConscriptFile, BSP_ROOT = None, RTT_ROOT = None):
     linkflags = rtconfig.M_LFLAGS + ' -e main'
     CPPPATH = GetCPPPATH(BSP_Root, Rtt_Root)
 
-    Env = Environment(tools = ['mingw'],
-        AS = rtconfig.AS, ASFLAGS = rtconfig.AFLAGS,
-        CC = rtconfig.CC, CCFLAGS = rtconfig.M_CFLAGS,
-        CXX = rtconfig.CXX, AR = rtconfig.AR, ARFLAGS = '-rc',
-        LINK = rtconfig.LINK, LINKFLAGS = linkflags,
-        CPPPATH = CPPPATH)
+    if rtconfig.PLATFORM == 'cl': 
+        Env = Environment(TARGET_ARCH='x86')
+        Env.Append(CCFLAGS=rtconfig.M_CFLAGS)
+        Env.Append(LINKFLAGS=rtconfig.M_LFLAGS)
+        Env.Append(CPPPATH=CPPPATH)
+        Env.Append(LIBS='rtthread', LIBPATH=BSP_Root)
+        Env.Append(CPPDEFINES=['RTT_IN_MODULE'])
+        Env.PrependENVPath('PATH', rtconfig.EXEC_PATH)
+    else:
+        Env = Environment(tools = ['mingw'],
+            AS = rtconfig.AS, ASFLAGS = rtconfig.AFLAGS,
+            CC = rtconfig.CC, CCFLAGS = rtconfig.M_CFLAGS,
+            CXX = rtconfig.CXX, AR = rtconfig.AR, ARFLAGS = '-rc',
+            LINK = rtconfig.LINK, LINKFLAGS = linkflags,
+            CPPPATH = CPPPATH)
 
     PrepareModuleBuilding(Env, Rtt_Root)
 
     objs = SConscript(SConscriptFile)
 
     # build program 
-    target = Env.Program(TARGET, objs)
+    if rtconfig.PLATFORM == 'cl':
+        dll_target = TARGET.replace('.mo', '.dll')
+        target = Env.SharedLibrary(dll_target, objs)
 
-    # Env.Command("$TARGET", target)
+        target = Command("$TARGET", dll_target, [Move(TARGET, dll_target)])
+        # target = dll_target
+    else:
+        target = Env.Program(TARGET, objs)
+
+    if hasattr(rtconfig, 'M_POST_ACTION'):
+        Env.AddPostAction(target, rtconfig.POST_ACTION)
+
+    if hasattr(rtconfig, 'M_BIN_PATH'):
+        Env.AddPostAction(target, [Copy(rtconfig.M_BIN_PATH, TARGET)])
