5a9420043372821d6780df730479e36286d5e908
Bernard Xiong <bernard.xiong@gmail.com>
2018-04-25 23:20:19 +0800

    Update ua.py

--- tools/ua.py
+++ tools/ua.py
@@ -60,25 +60,91 @@ def BuildEnv(BSP_ROOT, RTT_ROOT):
     BSP_Root = BSP_ROOT
 
 def BuildHostApplication(TARGET, SConscriptFile):
+    import platform
+
+    platform_type = platform.system()
+    if platform_type == 'Windows' or platform_type.find('MINGW') != -1:
+        TARGET = TARGET.replace('.mo', '.exe')
+
     sys.path = sys.path + [os.path.join(os.getcwd(), 'tools', 'host')]
 
     from building import PrepareHostModuleBuilding
 
+    HostRtt = os.path.join(os.getcwd(), 'tools', 'host', 'rtthread')
     Env = Environment()
+
+    if not GetOption('verbose'):
+        # override the default verbose command string
+        Env.Replace(
+            ARCOMSTR = 'AR $TARGET',
+            ASCOMSTR = 'AS $TARGET',
+            ASPPCOMSTR = 'AS $TARGET',
+            CCCOMSTR = 'CC $TARGET',
+            CXXCOMSTR = 'CXX $TARGET',
+            LINKCOMSTR = 'LINK $TARGET'
+        )
+
     PrepareHostModuleBuilding(Env)
+
     objs = SConscript(SConscriptFile)
+    objs += SConscript(HostRtt + '/SConscript')
+
     target = Env.Program(TARGET, objs)
     return
+
+def BuildHostLibrary(TARGET, SConscriptFile):
+    import platform
+
+    platform_type = platform.system()
+    if platform_type == 'Windows' or platform_type.find('MINGW') != -1:
+        TARGET = TARGET.replace('.mo', '.exe')
+
+    sys.path = sys.path + [os.path.join(os.getcwd(), 'tools', 'host')]
+
+    from building import PrepareHostModuleBuilding
+
+    HostRtt = os.path.join(os.getcwd(), 'tools', 'host', 'rtthread')
+    Env = Environment()
+
+    if not GetOption('verbose'):
+        # override the default verbose command string
+        Env.Replace(
+            ARCOMSTR = 'AR $TARGET',
+            ASCOMSTR = 'AS $TARGET',
+            ASPPCOMSTR = 'AS $TARGET',
+            CCCOMSTR = 'CC $TARGET',
+            CXXCOMSTR = 'CXX $TARGET',
+            LINKCOMSTR = 'LINK $TARGET'
+        )
+
+    PrepareHostModuleBuilding(Env)
+
+    objs = SConscript(SConscriptFile)
+    objs += SConscript(HostRtt + '/SConscript')
+
+    target = Env.Program(TARGET, objs)
+    return
+
 
 def BuildApplication(TARGET, SConscriptFile, BSP_ROOT = None, RTT_ROOT = None):
     global Env
     global Rtt_Root
     global BSP_Root
 
+    # add comstr option
+    AddOption('--verbose',
+                dest='verbose',
+                action='store_true',
+                default=False,
+                help='print verbose information during build')
+
     # build application in host 
     if BSP_ROOT == None and RTT_ROOT == None and not os.getenv('BSP_ROOT'):
         BuildHostApplication(TARGET, SConscriptFile)
         return
+
+    if RTT_ROOT == None and os.getenv('RTT_ROOT'):
+        RTT_ROOT = os.getenv('RTT_ROOT')
 
     # handle BSP_ROOT and RTT_ROOT
     BuildEnv(BSP_ROOT, RTT_ROOT)
@@ -88,6 +154,7 @@ def BuildApplication(TARGET, SConscriptFile, BSP_ROOT = None, RTT_ROOT = None):
     # get configuration from BSP 
     import rtconfig 
     from rtua import GetCPPPATH
+    from rtua import GetCPPDEFINES
     from building import PrepareModuleBuilding
 
     linkflags = rtconfig.M_LFLAGS + ' -e main'
@@ -99,15 +166,27 @@ def BuildApplication(TARGET, SConscriptFile, BSP_ROOT = None, RTT_ROOT = None):
         Env.Append(LINKFLAGS=rtconfig.M_LFLAGS)
         Env.Append(CPPPATH=CPPPATH)
         Env.Append(LIBS='rtthread', LIBPATH=BSP_Root)
-        Env.Append(CPPDEFINES=['RTT_IN_MODULE'])
+        Env.Append(CPPDEFINES=GetCPPDEFINES() + ['RTT_IN_MODULE'])
         Env.PrependENVPath('PATH', rtconfig.EXEC_PATH)
     else:
         Env = Environment(tools = ['mingw'],
             AS = rtconfig.AS, ASFLAGS = rtconfig.AFLAGS,
             CC = rtconfig.CC, CCFLAGS = rtconfig.M_CFLAGS,
+            CPPDEFINES = GetCPPDEFINES(),
             CXX = rtconfig.CXX, AR = rtconfig.AR, ARFLAGS = '-rc',
             LINK = rtconfig.LINK, LINKFLAGS = linkflags,
             CPPPATH = CPPPATH)
+
+    if not GetOption('verbose'):
+        # override the default verbose command string
+        Env.Replace(
+            ARCOMSTR = 'AR $TARGET',
+            ASCOMSTR = 'AS $TARGET',
+            ASPPCOMSTR = 'AS $TARGET',
+            CCCOMSTR = 'CC $TARGET',
+            CXXCOMSTR = 'CXX $TARGET',
+            LINKCOMSTR = 'LINK $TARGET'
+        )
 
     PrepareModuleBuilding(Env, Rtt_Root, BSP_Root)
 
@@ -124,7 +203,89 @@ def BuildApplication(TARGET, SConscriptFile, BSP_ROOT = None, RTT_ROOT = None):
         target = Env.Program(TARGET, objs)
 
     if hasattr(rtconfig, 'M_POST_ACTION'):
-        Env.AddPostAction(target, rtconfig.POST_ACTION)
+        Env.AddPostAction(target, rtconfig.M_POST_ACTION)
+
+    if hasattr(rtconfig, 'M_BIN_PATH'):
+        Env.AddPostAction(target, [Copy(rtconfig.M_BIN_PATH, TARGET)])
+
+def BuildLibrary(TARGET, SConscriptFile, BSP_ROOT = None, RTT_ROOT = None):
+    global Env
+    global Rtt_Root
+    global BSP_Root
+
+    # add comstr option
+    AddOption('--verbose',
+                dest='verbose',
+                action='store_true',
+                default=False,
+                help='print verbose information during build')
+
+    # build application in host 
+    if BSP_ROOT == None and RTT_ROOT == None and not os.getenv('BSP_ROOT'):
+        BuildHostLibrary(TARGET, SConscriptFile)
+        return
+
+    if RTT_ROOT == None and os.getenv('RTT_ROOT'):
+        RTT_ROOT = os.getenv('RTT_ROOT')
+
+    # handle BSP_ROOT and RTT_ROOT
+    BuildEnv(BSP_ROOT, RTT_ROOT)
+
+    sys.path = sys.path + [os.path.join(Rtt_Root, 'tools'), BSP_Root]
+
+    # get configuration from BSP 
+    import rtconfig 
+    from rtua import GetCPPPATH
+    from rtua import GetCPPDEFINES
+    from building import PrepareModuleBuilding
+
+    linkflags = rtconfig.M_LFLAGS + ' -e 0'
+    CPPPATH = GetCPPPATH(BSP_Root, Rtt_Root)
+
+    if rtconfig.PLATFORM == 'cl': 
+        Env = Environment(TARGET_ARCH='x86')
+        Env.Append(CCFLAGS=rtconfig.M_CFLAGS)
+        Env.Append(LINKFLAGS=rtconfig.M_LFLAGS)
+        Env.Append(CPPPATH=CPPPATH)
+        Env.Append(LIBS='rtthread', LIBPATH=BSP_Root)
+        Env.Append(CPPDEFINES=GetCPPDEFINES() + ['RTT_IN_MODULE'])
+        Env.PrependENVPath('PATH', rtconfig.EXEC_PATH)
+    else:
+        Env = Environment(tools = ['mingw'],
+            AS = rtconfig.AS, ASFLAGS = rtconfig.AFLAGS,
+            CC = rtconfig.CC, CCFLAGS = rtconfig.M_CFLAGS,
+            CPPDEFINES = GetCPPDEFINES(),
+            CXX = rtconfig.CXX, AR = rtconfig.AR, ARFLAGS = '-rc',
+            LINK = rtconfig.LINK, LINKFLAGS = linkflags,
+            CPPPATH = CPPPATH)
+
+    if not GetOption('verbose'):
+        # override the default verbose command string
+        Env.Replace(
+            ARCOMSTR = 'AR $TARGET',
+            ASCOMSTR = 'AS $TARGET',
+            ASPPCOMSTR = 'AS $TARGET',
+            CCCOMSTR = 'CC $TARGET',
+            CXXCOMSTR = 'CXX $TARGET',
+            LINKCOMSTR = 'LINK $TARGET'
+        )
+
+    PrepareModuleBuilding(Env, Rtt_Root, BSP_Root)
+
+    objs = SConscript(SConscriptFile)
+
+    # build program 
+    if rtconfig.PLATFORM == 'cl':
+        dll_target = TARGET.replace('.so', '.dll')
+        target = Env.SharedLibrary(dll_target, objs)
+
+        target = Command("$TARGET", dll_target, [Move(TARGET, dll_target)])
+        # target = dll_target
+    else:
+        target = Env.Program(TARGET, objs)
+
+    if hasattr(rtconfig, 'M_POST_ACTION'):
+        Env.AddPostAction(target, rtconfig.M_POST_ACTION)
 
     if hasattr(rtconfig, 'M_BIN_PATH'):
         Env.AddPostAction(target, [Copy(rtconfig.M_BIN_PATH, TARGET)])
